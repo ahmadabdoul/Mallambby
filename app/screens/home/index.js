6186668193abff59539103b371adf71ad11d2b20
Abdulkadir Ahmad <aabdulkadir109@gmail.com>
2024-10-23 07:11:30 +0100

    added statusbar to home index

--- app/screens/home/index.js
+++ app/screens/home/index.js
@@ -1,13 +1,14 @@
 import { colorsVar } from "../../utils/colors";
 import HeaderComponent from "../../components/header";
 import ContentComponent from "../../components/content";
-import { StyleSheet, SafeAreaView, Platform } from "react-native";
+import { StyleSheet, SafeAreaView, Platform, StatusBar } from "react-native";
 import { Link } from "expo-router";
 import Carousel from "../../components/carousel";
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={"dark-content"} />
       <HeaderComponent />
       <Carousel />
       <ContentComponent />
