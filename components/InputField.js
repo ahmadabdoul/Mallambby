1d91a9c080a3af25a6c41e1cff27227d07aded4e
Abdulkadir Ahmad <aabdulkadir109@gmail.com>
2024-10-31 04:53:06 +0000

    inputfield component

---
+++ components/InputField.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+export const InputField = ({ label, placeholder, keyboardType = 'default', onChangeText }) => (
+  <View style={styles.inputContainer}>
+    <Text style={styles.label}>{label}</Text>
+    <TextInput
+      style={styles.input}
+      placeholder={placeholder}
+      keyboardType={keyboardType}
+      onChangeText={onChangeText}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    backgroundColor: 'white',
+    padding: 10,
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 14,
+    color: 'gray',
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    borderRadius: 5,
+  },
+});
