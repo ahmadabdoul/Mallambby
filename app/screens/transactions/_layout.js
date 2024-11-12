d39c7848408c4c5f3c563942ca9f8e8befd825bb
Abdulkadir Ahmad <aabdulkadir109@gmail.com>
2024-11-12 16:08:34 +0000

    made header for transactions transparent

--- app/screens/transactions/_layout.js
+++ app/screens/transactions/_layout.js
@@ -5,8 +5,8 @@ import { colorsVar } from "../../utils/colors";
 const StackLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: true, title: "Transactions" }} />
-      <Stack.Screen name="receipt" options={{ headerShown: true, title: "Transaction Receipt" }} />
+      <Stack.Screen name="index" options={{ headerShown: true, title: "Transactions", headerBlurEffect: "regular", headerTransparent: true }} />
+      <Stack.Screen name="receipt" options={{ headerShown: true, title: "Transaction Receipt", headerBlurEffect: "regular", headerTransparent: true }} />
     </Stack>
   );
 };
